a65dc58c0a50fd8abeea077e9d28f3a537e9bcaa
Diogo Carvalho <34646795+Diogo-P-Carvalho@users.noreply.github.com>
2018-10-13 09:36:35 +0100

    files organizations and cleaning the code

--- src/Components/Scroll.js
+++ src/Components/Scroll.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Scroll = (props) => {
     //console.log(props);
     return (
-        <div style={{overflowY: 'scroll', border: '1px solid black', height: '700px'}}>
+        <div style={{overflowY: 'scroll', border: '3px solid black', height: '700px'}}>
             {props.children}
         </div>
     )
