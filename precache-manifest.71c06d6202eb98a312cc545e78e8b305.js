d915e9b729d290d475e732d66cb58ce11277d6c8
Diogo Carvalho <34646795+Diogo-P-Carvalho@users.noreply.github.com>
2019-10-21 23:04:40 +0100

    Updates

---
+++ precache-manifest.71c06d6202eb98a312cc545e78e8b305.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f2b2e8da404ad2c902f448c34e1c200",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "d3c1330e2247de3b1fc3",
+    "url": "/Robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0cae271fd528ce38c781",
+    "url": "/Robofriends/static/css/main.ee90382b.chunk.css"
+  },
+  {
+    "revision": "d3c1330e2247de3b1fc3",
+    "url": "/Robofriends/static/js/2.ccfa30dd.chunk.js"
+  },
+  {
+    "revision": "0cae271fd528ce38c781",
+    "url": "/Robofriends/static/js/main.1031adfa.chunk.js"
+  },
+  {
+    "revision": "78cab9b9619ddcd920c7",
+    "url": "/Robofriends/static/js/runtime-main.ff6ebadc.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
